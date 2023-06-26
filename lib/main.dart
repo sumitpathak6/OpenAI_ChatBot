d86aa5024c35ef8e0b5c0805f7af147d2e96bcbf
Sumit Pathak <sumit.pathak0606@gmail.com>
2023-06-26 12:29:55 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:allen/pallete.dart';
+import 'package:flutter/material.dart';
+import 'home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+          scaffoldBackgroundColor: Pallete.whiteColor,
+          appBarTheme: const AppBarTheme(backgroundColor: Pallete.whiteColor)),
+      title: 'Allen',
+      home: const HomePage(),
+    );
+  }
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,1 @@
+const openAIAPIKey = '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';
